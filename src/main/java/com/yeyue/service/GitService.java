8b0e17a98812f6ee57443b2aa3cc4de517d09297
tom_gob <goodMorning_glb@atguigu.com>
2020-03-06 23:42:34 +0800

    test conflict

--- src/main/java/com/yeyue/service/GitService.java
+++ src/main/java/com/yeyue/service/GitService.java
@@ -1,11 +1,10 @@
 package com.yeyue.service;
 
 public class GitService {
-	public static void main(String[] args) {
-		System.out.println("left");
-	}
+	
 
 	public static void main(String[] args) {
 		System.out.println("right");
+		System.out.println("left");
 	}
 }
